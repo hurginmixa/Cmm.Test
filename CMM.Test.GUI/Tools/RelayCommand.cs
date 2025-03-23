e048adfbb0339d834d3dce54d1878228be210a02
Michael Hurgin <mixah@camtek.com>
2025-03-23 19:31:50 +0200

    ~2

---
+++ CMM.Test.GUI/Tools/RelayCommand.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Input;
+
+namespace CMM.Test.GUI.Tools
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object?> _execute;
+        private readonly Func<bool>? _canExecute;
+
+        public RelayCommand(Action<object?> execute, Func<bool>? canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter) => _canExecute == null || _canExecute();
+
+        public void Execute(object? parameter) => _execute(parameter);
+    }
+}
